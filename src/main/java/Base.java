88fc4ea8cc282fd0fa2bec587253cc1b8d66bca4
ksavukov <savukovkirill12@gmail.com>
2018-05-06 22:45:28 +0300

    Добавлен Exception для задания 7

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -4,35 +4,40 @@ import Operations.*;
 
 
 public class Base {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        float f = scanner.nextFloat();
-        System.out.println("Введите второе число");
-        float fs = scanner.nextFloat();
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите первое число");
+            float f = scanner.nextFloat();
+            System.out.println("Введите второе число");
+            float fs = scanner.nextFloat();
 
-        System.out.println("Выберите операцию: ");
-        System.out.println("1 - Сложение");
-        System.out.println("2 - Вычитание");
-        System.out.println("3 - Деление");
-        System.out.println("4 - Умножение");
-        System.out.println("5 - Выход");
-        int operation = scanner.nextInt();
+            System.out.println("Выберите операцию: ");
+            System.out.println("1 - Сложение");
+            System.out.println("2 - Вычитание");
+            System.out.println("3 - Деление");
+            System.out.println("4 - Умножение");
+            System.out.println("5 - Выход");
+            int operation = scanner.nextInt();
 
-        interOperations Oper = new allOperations();
+            interOperations Oper = new allOperations();
 
-        if (operation == 1) {
-            System.out.printf("%.4f", Oper.sum(f, fs));
-        } else if (operation == 2) {
-            System.out.printf("%.4f", Oper.min(f, fs));
-        } else if (operation == 3) {
-            System.out.printf("%.4f", Oper.division(f, fs));
-        } else if (operation == 4) {
-            System.out.printf("%.4f", Oper.multiplication(f, fs));
+            if (operation == 1) {
+                System.out.printf("%.4f", Oper.sum(f, fs));
+            } else if (operation == 2) {
+                System.out.printf("%.4f", Oper.min(f, fs));
+            } else if (operation == 3) {
+                System.out.printf("%.4f", Oper.division(f, fs));
+            } else if (operation == 4) {
+                System.out.printf("%.4f", Oper.multiplication(f, fs));
+            } else {
+                System.exit(0);
+
+            }
         }
-        else {
-            System.exit(0);
-
+       catch(ArithmeticException ex){
+                System.out.println("Делить на 0 нельзя!!!");
+            }
         }
     }
-}
+
--- src/main/java/Operations/allOperations.java
+++ src/main/java/Operations/allOperations.java
@@ -9,12 +9,17 @@ public class allOperations implements interOperations {
         return f - fs;
     }
 
+    public float division(float f, float fs) {
+        if (fs == 0) {
+            throw new ArithmeticException();
+        }
+        return f / fs;
+    }
     public float multiplication(float f, float fs) {
+
         return f * fs;
     }
 
-    public float division(float f, float fs) {
-        return f / fs;
-    }
+
 
 }
